65041d90254bc449ddd168764b01993b7a670ec5
Jamie Evans <evansjamie1122@gmail.com>
2024-09-30 10:32:52 -0400

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,26 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const item = action.payload;
+        const existingItem = state.items.find(i => i.name === item.name);
+
+        if (existingItem){
+            existingItem.quantity +=1;
+        } else {
+            state.items.push({ ...item, quantity: 1});
+        }
     },
     removeItem: (state, action) => {
+        const itemName = action.payload;
+        state.items = state.items.filter(i => i.name !== itemName);
     },
     updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
+        const item = state.items.find(i => i.name === name);
 
-    
+        if (item) {
+            item.quantity = quantity;
+        }
     },
   },
 });
